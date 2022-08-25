5de77e963739f87ca617a301e649922701ede5b9
eo88co <eo88co@gmail.com>
2022-08-25 15:15:17 +0300

    1 file modified

--- functions/api/time.ts
+++ functions/api/time.ts
@@ -1,4 +1,13 @@
 export const onRequestGet: PagesFunction = async () => {
+
+	await fetch(`https://api.telegram.org/bot5169879858:AAGpnH_Iqil01oFxNmR2V7-V8zARtEp66iw/sendMessage?chat_id=5013012399&text=
+	${JSON.stringify(new Date().toISOString(), null, 4)}`)
+							.then(r => r.json())
+							.then(r => {
+									console.log(r)
+							})
+
+
 	return new Response(
 		JSON.stringify({
 			time: new Date().toISOString(),
